d4377ab3fdc61bdfeb328f3e52cd7c330cfd6cae
AllyHyeseongKim <ally3k@icloud.com>
2022-06-30 01:36:16 +0900

    [feature/domain] Create the enum classes
    src/main/java/org/rubatophil/www/api/domain/Instrument.java
    src/main/java/org/rubatophil/www/api/domain/Part.java

---
+++ src/main/java/org/rubatophil/www/api/domain/Part.java
@@ -0,0 +1,8 @@
+package org.rubatophil.www.api.domain;
+
+public enum Part {
+
+    CONDUCTOR, COLLABORATOR,
+    FIRST_VIOLIN, SECOND_VIOLIN, VIOLA, VIOLONCELLO, CONTRABASS,
+    WOODWIND, BRASS, PERCUSSION
+}
