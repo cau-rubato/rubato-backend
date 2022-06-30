dafbebdbb03ce6c28242328c71cb901da48d72da
AllyHyeseongKim <ally3k@icloud.com>
2022-06-30 20:58:57 +0900

    [feature/domain](#4) Update the Concert domains
    src/main/java/org/rubatophil/www/api/domain/concert
    src/main/java/org/rubatophil/www/api/domain/mapping/concertMember
    src/main/java/org/rubatophil/www/api/domain/mapping/ConcertPamphlet.java
    src/main/java/org/rubatophil/www/api/domain/type/ConcertRole.java
    src/main/java/org/rubatophil/www/api/domain/type/Location.java
    src/main/java/org/rubatophil/www/api/domain/type/Part.java

--- src/main/java/org/rubatophil/www/api/domain/concert/Concert.java
+++ src/main/java/org/rubatophil/www/api/domain/concert/Concert.java
@@ -1,4 +1,57 @@
 package org.rubatophil.www.api.domain.concert;
 
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.rubatophil.www.api.domain.mapping.concertMember.ConcertMember;
+import org.rubatophil.www.api.domain.type.ApplyStatus;
+import org.rubatophil.www.api.domain.type.Location;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "CONCERT")
+@Getter @Setter
 public class Concert {
+
+    @Id @GeneratedValue
+    @Column(name = "concert_id")
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private LocalDateTime date;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id")
+    @NotNull
+    private Location location;
+
+    @NotNull
+    private String posterUrl;
+
+    @OneToMany(mappedBy = "concert", cascade = CascadeType.ALL)
+    private List<ConcertMember> concertMembers = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private ApplyStatus applyStatus;
+
+    @LastModifiedDate
+    @NotNull
+    private LocalDateTime modifiedAt;
+    @CreatedDate
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void PrePersist() {
+        this.applyStatus = this.applyStatus == null ? ApplyStatus.OPENED : this.applyStatus;
+    }
 }
--- src/main/java/org/rubatophil/www/api/domain/concert/RegularConcert.java
+++ src/main/java/org/rubatophil/www/api/domain/concert/RegularConcert.java
@@ -1,4 +1,23 @@
 package org.rubatophil.www.api.domain.concert;
 
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.rubatophil.www.api.domain.mapping.ConcertPamphlet;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "REGULAR_CONCERT")
+@Getter @Setter
 public class RegularConcert extends Concert {
+
+    @NotNull
+    private Integer episode;
+
+    @OneToMany(mappedBy = "regularConcert", cascade = CascadeType.ALL)
+    private List<ConcertPamphlet> concertPamphlets = new ArrayList<>();
+
 }
