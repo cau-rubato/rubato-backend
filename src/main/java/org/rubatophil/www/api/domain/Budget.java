3c2bc6e0cf4e41b073d93b3e5091e0b677a398a0
AllyHyeseongKim <ally3k@icloud.com>
2022-06-30 21:00:32 +0900

    [feature/domain] Update the FAQ and Donate domains
    src/main/java/org/rubatophil/www/api/domain/Budget.java
    src/main/java/org/rubatophil/www/api/domain/Donate.java
    src/main/java/org/rubatophil/www/api/domain/FAQ.java

--- src/main/java/org/rubatophil/www/api/domain/Budget.java
+++ src/main/java/org/rubatophil/www/api/domain/Budget.java
@@ -1,4 +1,33 @@
 package org.rubatophil.www.api.domain;
 
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "BUDGET")
+@Getter @Setter
 public class Budget {
+
+    @Id @GeneratedValue
+    @Column(name = "budget_id")
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Donate donate;
+
+    @LastModifiedDate
+    @NotNull
+    private LocalDateTime modifiedAt;
+    @CreatedDate
+    @NotNull
+    private LocalDateTime createdAt;
 }
