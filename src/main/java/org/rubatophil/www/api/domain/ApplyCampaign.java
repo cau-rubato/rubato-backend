6232f009ea0235950c3b317861e8e374941da26c
AllyHyeseongKim <ally3k@icloud.com>
2022-07-12 17:04:23 +0900

    [fix/apply_domain](#34) Update the unit tests of Apply domains
    src/main/java/org/rubatophil/www/api/domain/Apply.java
    src/main/java/org/rubatophil/www/api/domain/ApplyCampaign.java
    src/test/java/org/rubatophil/www/api/domain/ApplyTest.java
    src/test/java/org/rubatophil/www/api/domain/ApplyCampaignTest.java

--- src/main/java/org/rubatophil/www/api/domain/ApplyCampaign.java
+++ src/main/java/org/rubatophil/www/api/domain/ApplyCampaign.java
@@ -1,7 +1,6 @@
 package org.rubatophil.www.api.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.rubatophil.www.api.domain.type.ApplyStatus;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -9,14 +8,18 @@ import org.springframework.data.annotation.LastModifiedDate;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "APPLY_CAMPAIGN")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ApplyCampaign {
 
     @Id @GeneratedValue
     @Column(name = "apply_campaign_id")
+    @Setter(AccessLevel.NONE)
     private Long id;
 
     @NotNull
@@ -31,13 +34,29 @@ public class ApplyCampaign {
     @NotNull
     private ApplyStatus status;
 
-    @OneToOne(mappedBy = "applyCampaign")
-    private Apply apply;
+    @OneToMany(mappedBy = "applyCampaign", cascade = CascadeType.ALL)
+    @Setter(AccessLevel.NONE)
+    private List<Apply> applies = new ArrayList<>();
 
     @LastModifiedDate
+    @Setter(AccessLevel.NONE)
     private LocalDateTime modifiedAt;
     @CreatedDate
+    @Setter(AccessLevel.NONE)
     private LocalDateTime createdAt;
+
+    @Builder
+    public ApplyCampaign(Integer generation, LocalDateTime openedAt, LocalDateTime closedAt, ApplyStatus status) {
+        this.generation = generation;
+        this.openedAt = openedAt;
+        this.closedAt = closedAt;
+        this.status = status;
+    }
+
+    public void addApply(Apply apply) {
+        this.applies.add(apply);
+        apply.setApplyCampaign(this);
+    }
 
     @PrePersist
     public void PrePersist() {
