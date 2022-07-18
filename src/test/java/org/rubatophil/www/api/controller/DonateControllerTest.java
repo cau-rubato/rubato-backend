e205f81371ffe62b8b7ba63dc8edd4ed1847b7f8
AllyHyeseongKim <ally3k@icloud.com>
2022-07-18 16:37:31 +0900

    [feature/faq](#7) Remove the unused libraries
    src/test/java/org/rubatophil/www/api/controller/DonateControllerTest.java
    src/test/java/org/rubatophil/www/api/controller/FAQControllerTest.java

--- src/test/java/org/rubatophil/www/api/controller/DonateControllerTest.java
+++ src/test/java/org/rubatophil/www/api/controller/DonateControllerTest.java
@@ -5,9 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
